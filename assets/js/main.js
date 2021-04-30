afae8c97ada7da4bcdcd85e5680b308cba330418
happyBirthaday <83461123+happyBirthaday@users.noreply.github.com>
2021-04-30 19:43:02 +0430

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+var container = document.getElementById("container");
+
+function resize() {
+    // the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
+
+if (typeof window.innerWidth != 'undefined') {
+    viewportwidth = window.innerWidth,
+    viewportheight = window.innerHeight
+}
+
+// IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
+
+else if (typeof document.documentElement != 'undefined' && typeof document.documentElement.clientWidth != 'undefined' && document.documentElement.clientWidth != 0) {
+    viewportwidth = document.documentElement.clientWidth,
+    viewportheight = document.documentElement.clientHeight
+}
+
+// older versions of IE
+
+else {
+    viewportwidth = document.getElementsByTagName('body')[0].clientWidth,
+    viewportheight = document.getElementsByTagName('body')[0].clientHeight
+}
+container.style.height = viewportheight+"px";
+container.style.width = viewportwidth+"px";
+}
